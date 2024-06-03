20f877804025b93eb6a19492516dec9e622cd644
AdrienBlanco <adrienblanco89@gmail.com>
2024-06-03 17:24:45 +0200

    feat(Hero): ajout des boutons dans le hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,18 @@
 import Section from '../../components/Section/Section';
+import ButtonOrLink from '../ButtonOrLink/ButtonOrLink';
 
 export default function Hero() {
 	return (
 		<Section className="hero">
 			<h1>Test</h1>
+			<div>
+			<ButtonOrLink isLink={true} href="https://www.example.com">
+				Go to Example.com
+			</ButtonOrLink>
+			<ButtonOrLink btnClassName={'reverse'} isLink={true} href="https://www.example.com">
+				Go to Example.com
+			</ButtonOrLink>
+			</div>
 		</Section>
 	);
 }
