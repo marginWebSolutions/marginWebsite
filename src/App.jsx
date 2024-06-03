5ba52b326be341eb27c4053e705d03213e911d48
AdrienBlanco <adrienblanco89@gmail.com>
2024-06-03 17:02:41 +0200

    style(Ajout des styles de bouton)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import ButtonOrLink from './components/ButtonOrLink/ButtonOrLink';
 import Section from './components/Section/Section';
 
 export default function App() {
 	return (
-		<p>
+		<div className='App'>
 			<Section className="test">
 				<h1>Test</h1>
+				<ButtonOrLink isLink={true} href="https://www.example.com">
+					Go to Example.com
+				</ButtonOrLink>
+				<ButtonOrLink btnClassName={'reverse'} isLink={true} href="https://www.example.com">
+					Go to Example.com
+				</ButtonOrLink>
 			</Section>
-		</p>
+		</div>
 	);
 }
