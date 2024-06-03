3d01385ca68377294f66b987be3a748ab855467b
AdrienBlanco <adrienblanco89@gmail.com>
2024-06-03 22:51:17 +0200

    style(buttons, Hero): création de la bordure gradient sur les boutons

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,20 @@
 import Section from '../../components/Section/Section';
 import ButtonOrLink from '../ButtonOrLink/ButtonOrLink';
+import "./Hero.scss";
 
 export default function Hero() {
 	return (
 		<Section className="hero">
-			<h1>Test</h1>
-			<div>
-			<ButtonOrLink isLink={true} href="/contact">
-				Nous Contacter
-			</ButtonOrLink>
-			<ButtonOrLink btnClassName={'reverse'} isLink={true} href="/services">
-				Nos services et tarifs
-			</ButtonOrLink>
+			<div className="hero__content">
+				<h1 className='hero__title'>Création et développement de sites Web à destination des PME et des artisans à Nevers et Saint-Etienne</h1>
+				<div className="hero__btn-wrapper">
+					<ButtonOrLink isLink={true} href="/contact">
+						Nous Contacter
+					</ButtonOrLink>
+					<ButtonOrLink btnClassName={'reverse'} isLink={true} href="/services">
+						Nos services et tarifs
+					</ButtonOrLink>
+				</div>
 			</div>
 		</Section>
 	);
