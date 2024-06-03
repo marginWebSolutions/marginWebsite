28ea1e5db63e8e824a4d1df497356177cda9ea78
Naoh <27915369+N40h@users.noreply.github.com>
2024-06-03 15:47:18 +0200

    chore: Update prop-types dependency to version 15.8.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+import Section from './component/Section/Section';
+
 export default function App() {
-	return <p>app</p>;
+	return (
+		<p>
+			<Section className="test">
+				<h1>Test</h1>
+			</Section>
+		</p>
+	);
 }
--- src/component/Section/Section.jsx
+++ src/component/Section/Section.jsx
@@ -1,6 +1,15 @@
-/* eslint-disable react/prop-types */
+import PropTypes from 'prop-types';
 import './Section.scss';
 
-export default function Section({ className }) {
-	return <section className={`section ${className}`}>section</section>;
+export default function Section({ className, children, ...props }) {
+	return (
+		<section className={`section ${className}`} {...props}>
+			{children}
+		</section>
+	);
 }
+
+Section.propTypes = {
+	className: PropTypes.string,
+	children: PropTypes.node.isRequired,
+};
