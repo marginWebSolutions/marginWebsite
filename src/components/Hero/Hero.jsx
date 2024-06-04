65fd6b861a989650a14142409f89368395a2ee43
Naoh <27915369+N40h@users.noreply.github.com>
2024-06-04 11:33:56 +0200

    chore: Update padding and remove unused code in Header and Navbar
    feat: Add gradient color mixin for text
    
    chore: Update font imports in index.scss
    
    chore: Update Hero component with logo, title, buttons, and image
    
    chore: Refactor ButtonOrLink component and update file structure

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,21 +1,47 @@
 import Section from '../../components/Section/Section';
 import ButtonOrLink from '../ButtonOrLink/ButtonOrLink';
-import "./Hero.scss";
+import './Hero.scss';
 
 export default function Hero() {
 	return (
-		<Section className="hero">
-			<div className="hero__content">
-				<h1 className='hero__title'>Création et développement de sites Web à destination des PME et des artisans à Nevers et Saint-Etienne</h1>
-				<div className="hero__btn-wrapper">
-					<ButtonOrLink isLink={true} href="/contact">
-						Nous Contacter
-					</ButtonOrLink>
-					<ButtonOrLink btnClassName={'reverse'} isLink={true} href="/services">
-						Nos services et tarifs
-					</ButtonOrLink>
+		<>
+			<Section className="hero">
+				<div className="hero__content">
+					<div className="hero__logo--container">
+						<p className="hero__logo">
+							<span className="hero__logo--ma m">m</span>
+							<span className="hero__logo--ma a">a</span>
+							RGIN
+						</p>
+						<p className="hero__logo--text">Web Solutions</p>
+					</div>
+					<h1 className="hero__title">
+						Création et développement de sites Web professionnels à
+						destination des PME et des artisans à Nevers et
+						Saint-Etienne
+					</h1>
+					<div className="hero__btn-wrapper">
+						<ButtonOrLink isLink={true} href="/contact">
+							Nous Contacter
+						</ButtonOrLink>
+						<ButtonOrLink
+							btnClassName={'reverse'}
+							isLink={true}
+							href="/services"
+						>
+							Nos services et tarifs
+						</ButtonOrLink>
+					</div>
 				</div>
-			</div>
-		</Section>
+				<div className="hero__image--container">
+					<img
+						src="/margin_hero.png"
+						alt="Illustration de site internet sur différents écrans"
+						className="hero__image"
+					/>
+				</div>
+			</Section>
+			<div className="underline"></div>
+		</>
 	);
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,7 +13,6 @@ export default function Navbar() {
 					itemText="Contact"
 					href="/contact"
 					className="btn btn__reverse"
-					data="Contact"
 				/>
 			</ul>
 		</nav>
