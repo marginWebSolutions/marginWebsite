6aaa8098696642cfdc3d40fb44e7910ce7343a62
AdrienBlanco <adrienblanco89@gmail.com>
2024-06-09 15:29:21 +0200

    fix(ClientsCard): Default size desktop

--- src/components/ClientsCard/ClientsCard.jsx
+++ src/components/ClientsCard/ClientsCard.jsx
@@ -8,7 +8,7 @@ export default function ClientsCard({ href, src, alt, text }) {
 				<div
 					className="clients__card--overlay"
 				></div>
-				<img src={src} alt={alt} className="clients__card--img" />
+				<img src={src} alt={alt} className="clients__card--img" width={432} height={310}/>
 			</a>
 			<p className="clients__card--text">{text}</p>
 		</div>
