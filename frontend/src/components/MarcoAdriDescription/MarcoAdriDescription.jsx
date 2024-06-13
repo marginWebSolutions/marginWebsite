269c41534749a98488bb3276d58b81d98301ec23
AdrienBlanco <adrienblanco89@gmail.com>
2024-06-13 16:11:05 +0200

    style(MarcoAdri): réutilisation du style du logo dans les noms

--- frontend/src/components/MarcoAdriDescription/MarcoAdriDescription.jsx
+++ frontend/src/components/MarcoAdriDescription/MarcoAdriDescription.jsx
@@ -16,7 +16,7 @@ export default function MarcoAdriDescription() {
 						<img className="ma-description__image ma-description__image--left" src="./marcoadri.png" alt="Marc" />
 					</div>
 					<div className="ma-description__wrapper ma-description__wrapper--left">
-						<h3 className="ma-description__title ma-description__title--left"><span className="margin-m">m</span>arc</h3>
+						<h3 className="ma-description__title ma-description__title--left names"><span className="margin-m">m</span>arc</h3>
 						<p className="ma-description__text ma-description__text--left">Margin, c’est l’histoire de deux amis passionnés par la création et le développement de sites Internet, qui décident de mettre leur savoir-faire au service des PME et des artisans autour de chez eux.</p>
 					</div>
 				</div>
@@ -29,7 +29,7 @@ export default function MarcoAdriDescription() {
 						<img className="ma-description__image ma-description__image--right" src="./marcoadri.png" alt="Adrien" />
 					</div>
 					<div className="ma-description__wrapper ma-description__wrapper--right">
-						<h3 className="ma-description__title ma-description__title--right"><span className="margin-a">a</span>drien</h3>
+						<h3 className="ma-description__title ma-description__title--right names"><span className="margin-a">a</span>drien</h3>
 						<p className="ma-description__text ma-description__text--right">Margin, c’est l’histoire de deux amis passionnés par la création et le développement de sites Internet, qui décident de mettre leur savoir-faire au service des PME et des artisans autour de chez eux.</p>
 					</div>
 				</div>
@@ -39,11 +39,11 @@ export default function MarcoAdriDescription() {
 					</div>
 					<div className="ma-description__wrappers--small">
 						<div className="ma-description__wrapper--small">
-							<h3 className="ma-description__title--small"><span className="margin-m">m</span>arc</h3>
+							<h3 className="ma-description__title--small names"><span className="margin-m">m</span>arc</h3>
 							<p className="ma-description__text--small">Margin, c’est l’histoire de deux amis passionnés par la création et le développement de sites Internet, qui décident de mettre leur savoir-faire au service des PME et des artisans autour de chez eux.</p>
 						</div>
 						<div className="ma-description__wrapper--small">
-							<h3 className="ma-description__title--small"><span className="margin-a">a</span>drien</h3>
+							<h3 className="ma-description__title--small names"><span className="margin-a">a</span>drien</h3>
 							<p className="ma-description__text--small">Margin, c’est l’histoire de deux amis passionnés par la création et le développement de sites Internet, qui décident de mettre leur savoir-faire au service des PME et des artisans autour de chez eux.</p>
 						</div>
 					</div>
