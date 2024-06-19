be68502afa52e138b742bd32d4b8263fc40a61bb
Naoh <27915369+N40h@users.noreply.github.com>
2024-06-19 14:07:04 +0200

    chore: Update favicon and helmet components

--- frontend/src/helmet/Favicon.jsx
+++ frontend/src/helmet/Favicon.jsx
@@ -7,24 +7,21 @@ export default function Favicon() {
 				<link
 					rel="apple-touch-icon"
 					sizes="180x180"
-					href={`${process.env.PUBLIC_URL}/img/favicon/apple-touch-icon.png`}
+					href={`/favicon/apple-touch-icon.png`}
 				/>
 				<link
 					rel="icon"
 					type="image/png"
 					sizes="32x32"
-					href={`${process.env.PUBLIC_URL}/img/favicon/favicon-32x32.png`}
+					href={`/favicon/favicon-32x32.png`}
 				/>
 				<link
 					rel="icon"
 					type="image/png"
 					sizes="16x16"
-					href={`${process.env.PUBLIC_URL}/img/favicon/favicon-16x16.png`}
+					href={`/favicon/favicon-16x16.png`}
 				/>
-				<link
-					rel="manifest"
-					href={`${process.env.PUBLIC_URL}/img/favicon/site.webmanifest`}
-				/>
+				<link rel="manifest" href={`/favicon/site.webmanifest`} />
 			</Helmet>
 		</>
 	);
