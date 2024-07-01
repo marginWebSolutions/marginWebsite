1df4120c27c9a0fd61831feaebf030cbfb0c9373
Naoh <27915369+N40h@users.noreply.github.com>
2024-07-01 14:37:12 +0200

    style: optimize image loading in Hero component

--- frontend/src/pages/Home/Hero/Hero.jsx
+++ frontend/src/pages/Home/Hero/Hero.jsx
@@ -5,9 +5,18 @@ import './Hero.scss';
 
 export default function Hero() {
 	const images = [
-		{ src: './margin_web_solutions_navigation_site_Internet.webp', alt: 'Navigation fluide sur un site Internet' },
-		{ src: './margin_web_solutions_maquettage.webp', alt: "Création de maquette et wireframe d'un site Web" },
-		{ src: './margin_web_solutions_developpement_site_web.webp', alt: 'développement de site web pour PME et Artisans à Nevers et Saint-Etienne' },
+		{
+			src: './margin_web_solutions_navigation_site_Internet.webp',
+			alt: 'Navigation fluide sur un site Internet',
+		},
+		{
+			src: './margin_web_solutions_maquettage.webp',
+			alt: "Création de maquette et wireframe d'un site Web",
+		},
+		{
+			src: './margin_web_solutions_developpement_site_web.webp',
+			alt: 'développement de site web pour PME et Artisans à Nevers et Saint-Etienne',
+		},
 	];
 
 	const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -50,19 +59,25 @@ export default function Hero() {
 				</div>
 				<div className="hero__container">
 					<div className="hero__content">
+						<h1 className="hero__title">
+							Création et développement de sites Web
+							professionnels à destination des PME et des artisans
+							à Nevers et Saint-Etienne
+						</h1>
 						<div className="hero__logo--container">
 							<span className="hero__logo">
 								<span className="hero__logo--ma m">m</span>
 								<span className="hero__logo--ma a">a</span>
 								RGIN
 							</span>
-							<span className="hero__logo--text">Web <span className='hero__logo--text gradient'>Solutions</span></span>
+							<span className="hero__logo--text">
+								Web{' '}
+								<span className="hero__logo--text gradient">
+									Solutions
+								</span>
+							</span>
 						</div>
-						<h1 className="hero__title">
-							Création et développement de sites Web
-							professionnels à destination des PME et des artisans
-							à Nevers et Saint-Etienne	
-						</h1>
+
 						<ButtonDoubled
 							btnFirst={'Nous Contacter'}
 							hrefFirst={'/contact'}
