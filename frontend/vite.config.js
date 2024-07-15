f0b259ee4e0b7098338e9cdc5603679039b53670
Naoh <27915369+N40h@users.noreply.github.com>
2024-07-15 16:59:21 +0200

    chore: Update parserOptions in .eslintrc.cjs to use latest ECMAScript version

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,4 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	esbuild: {
-		target: 'es2020',
-	},
-	build: {
-		target: 'es2020',
-	},
 });
